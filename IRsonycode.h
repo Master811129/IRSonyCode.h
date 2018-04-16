cbb15a01c4c7c725cce3a8dd2836b5c255435fd4
Master811129 <37829730+Master811129@users.noreply.github.com>
2018-04-16 21:32:33 +0430

    Add files via upload

---
+++ IRsonycode.h
@@ -0,0 +1,41 @@
+//                   *In the name of God*
+/***********************************************************|
+|                                                           |
+|                   Powered By Master                       |
+|                                                           |
+************************************************************/
+               #define    POWER      0xa90
+               #define      B0       0x910
+               #define      B1       0x10
+               #define      B2       0x810
+               #define      B3       0x410 
+               #define      B4       0xc10
+               #define      B5       0x210
+               #define      B6       0xa10
+               #define      B7       0x610
+               #define      B8       0xe10
+               #define      B9       0x110
+               #define    VOL_UP     0x490
+               #define   VOLUME_UP   0x490
+               #define   VOL_DOWN    0xc90
+               #define  VOLUME_DOWN  0Xc90
+               #define  CHANNEL_UP   0x90
+               #define CHANNEL_DOWN  0x890 
+               #define  PICTURE_UP   0x190
+               #define PICTURE_DOWN  0x990 
+               #define   TVVIDEO    0xA50
+               #define    CLEAR      0x2d0
+               #define     JUMP      0xdd0
+               #define     MTS       0xe90
+               #define   DISPLAY     0x5d0
+               #define    SLEEP      0x5d0 
+               #define     MUTE      0x290
+               #define    REWIND     0xd9a
+               #define     PLAY      0x59a 
+               #define     PAUSE     0x99a
+               #define     STOP      0x19a
+               #define    RECORD     0xb9a
+               #define     FF        0x39a
+               #define     ENTER     0xd10 
+               #define     EJECT     0x69a
+               #define      BIT        12
